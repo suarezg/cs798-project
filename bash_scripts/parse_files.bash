ff67a794dcf3721ac3c67c95a6a6835d43fca806
Gin Suarez <ginnsuarez@gmail.com>
2021-04-23 22:25:50 -0400

    Updated parse_files.bash to parse specific results (using different ordered sets)

--- bash_scripts/parse_files.bash
+++ bash_scripts/parse_files.bash
@@ -7,16 +7,32 @@
 #
 
 
+alg=$1
+
 cd ..
 
-file_out="results.csv"
+if  [ "$alg" == A ]
+then
+    file_out="results_AVL.csv"
+    file_prefix="data/AVL"
+elif [ "$alg" == L ]
+then
+    file_out="results_LL.csv"
+    file_prefix="data/LinkedList"
+elif [ "$alg" == R ]
+then
+    file_out="results_RB.csv"
+    file_prefix="data/RedBlack"
+fi
+
+
 
 rm -f "$file_out"
 
 # create header
-(cat data/CATree*_1.txt | grep -E "^(totalThreads|keyRangeSize|insertPercent|deletePercent|millisToRun|completedOperations|throughput)=[A-Za-z0-9\.]+$" | cut -d"=" -f1 | tr "\n" "," | sed 's/.$//'; echo) >> "$file_out"
+(cat "$file_prefix"/CATree*_1.txt | grep -E "^(totalThreads|keyRangeSize|insertPercent|deletePercent|millisToRun|completedOperations|throughput)=[A-Za-z0-9\.]+$" | cut -d"=" -f1 | tr "\n" "," | sed 's/.$//'; echo) >> "$file_out"
 
-for file in data/CATree*.txt; do
+for file in "$file_prefix"/CATree*.txt; do
     ./bash_scripts/parse_file.bash "$file" >> "$file_out"
 done
 
